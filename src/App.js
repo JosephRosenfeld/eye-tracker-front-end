e191201c0ad7ff3bd266030d6d88b41731a5e54b
Joseph <josephgrosenfeld@gmail.com>
2021-12-16 14:45:35 -0500

    fixed routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+//component imports
+import ThreeDay from "./screens/ThreeDay";
+import Weekly from "./screens/Weekly";
+import Yearly from "./screens/Yearly";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
       <div className='App'>
         <Header />
-        <Switch>
-          <Route path='/' component={Weekly} />
-          <Route path='/3day' component={ThreeDay} />
-          <Route path='/week' component={Weekly} />
-          <Route path='/year' component={Yearly} />
-        </Switch>
+        <Routes>
+          <Route path='/3day' element={<ThreeDay />} />
+          <Route path='/week' element={<Weekly />} />
+          <Route path='/year' element={<Yearly />} />
+        </Routes>
       </div>
     </Router>
   );
