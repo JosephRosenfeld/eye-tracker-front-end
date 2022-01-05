5f0fa373dee8b21f612b5f37a7b033eda1faaee3
Joseph <josephgrosenfeld@gmail.com>
2022-01-05 14:59:59 -0500

    focus test

--- src/components/DateViewer.js
+++ src/components/DateViewer.js
@@ -72,7 +72,6 @@ const DateViewer = ({ dateVisible }) => {
     >
       <span
         className='left-arrow material-icons'
-        tabIndex={1}
         onClick={() => changeViewDt(-1)}
       >
         chevron_left
@@ -80,7 +79,6 @@ const DateViewer = ({ dateVisible }) => {
       {dateVisible && <div className='view-date'>{showDate}</div>}
       <span
         className='right-arrow material-icons'
-        tabIndex={2}
         onClick={() => changeViewDt(1)}
       >
         chevron_right
