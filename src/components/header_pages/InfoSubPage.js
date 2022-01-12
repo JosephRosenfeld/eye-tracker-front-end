83a6bdebcef80e9e7a35afc10a492ddd1e7bff4b
Joseph <josephgrosenfeld@gmail.com>
2022-01-12 12:57:04 -0500

    finished info page

--- src/components/header_pages/InfoSubPage.js
+++ src/components/header_pages/InfoSubPage.js
@@ -12,7 +12,36 @@ const InfoSubPage = () => {
           pariatur elit deserunt veniam. Ad non in in officia pariatur eu irure
           veniam cillum sint dolor fugiat laboris.
         </div>
-        <div className='info-subtitle'></div>
+        <div className='info-text'>
+          Sit laborum reprehenderit dolore occaecat excepteur reprehenderit.
+          Occaecat proident magna laborum ipsum nisi cupidatat deserunt aliquip
+          Lorem nostrud id magna anim. Veniam et laborum adipisicing quis do
+          pariatur elit deserunt veniam. Ad non in in officia pariatur eu irure
+          veniam cillum sint dolor fugiat laboris.
+        </div>
+        <div className='info-subtitle'>Sub Title</div>
+        <div className='info-text'>
+          Tempor excepteur ex qui ex enim aliquip occaecat commodo exercitation.
+          Ut exercitation labore ex eu officia enim mollit occaecat cillum anim
+          eu. Quis dolore elit id amet. Exercitation anim pariatur aute velit
+          labore ipsum consectetur.
+        </div>
+        <div className='info-title'>Info Title</div>
+        <div className='info-text'>
+          Sit laborum reprehenderit dolore occaecat excepteur reprehenderit.
+          Occaecat proident magna laborum ipsum nisi cupidatat deserunt aliquip
+          Lorem nostrud id magna anim. Veniam et laborum adipisicing quis do
+          pariatur elit deserunt veniam. Ad non in in officia pariatur eu irure
+          veniam cillum sint dolor fugiat laboris.
+        </div>
+        <div className='info-text'>
+          Sit laborum reprehenderit dolore occaecat excepteur reprehenderit.
+          Occaecat proident magna laborum ipsum nisi cupidatat deserunt aliquip
+          Lorem nostrud id magna anim. Veniam et laborum adipisicing quis do
+          pariatur elit deserunt veniam. Ad non in in officia pariatur eu irure
+          veniam cillum sint dolor fugiat laboris.
+        </div>
+        <div className='info-subtitle'>Sub Title</div>
         <div className='info-text'>
           Tempor excepteur ex qui ex enim aliquip occaecat commodo exercitation.
           Ut exercitation labore ex eu officia enim mollit occaecat cillum anim
