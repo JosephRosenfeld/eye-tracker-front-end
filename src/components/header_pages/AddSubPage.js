1981ddffdc55b7779f0c99ccf757e2dc7a0d42df
Joseph <josephgrosenfeld@gmail.com>
2022-01-13 15:35:35 -0500

    added add page

--- src/components/header_pages/AddSubPage.js
+++ src/components/header_pages/AddSubPage.js
@@ -1,5 +1,32 @@
+import "./AddSubPage.css";
+
+/*--- Hooks imports ---*/
+import { useState } from "react";
+
 const AddSubPage = () => {
-  return <div></div>;
+  return (
+    <div className='add-item-container'>
+      <form className='add-item-content'>
+        <div className='add-item-title'>Item Info</div>
+        <div className='add-form-item'>
+          <label>TYPE</label>
+          <input type='text'></input>
+        </div>
+        <div className='add-form-item'>
+          <label>DATE</label>
+          <input type='date'></input>
+        </div>
+        <div className='add-form-item'>
+          <label>TIME</label>
+          <input type='time'></input>
+        </div>
+        <div className='add-form-item'>
+          <label>NOTES</label>
+          <textarea name='notes'></textarea>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default AddSubPage;
