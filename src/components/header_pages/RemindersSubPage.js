71b3aa4d3c10611e2a89f52aa0cf776181ced5ca
Joseph <josephgrosenfeld@gmail.com>
2022-01-13 13:52:19 -0500

    switched to select

--- src/components/header_pages/RemindersSubPage.js
+++ src/components/header_pages/RemindersSubPage.js
@@ -78,12 +78,20 @@ const RemindersSubPage = () => {
               <option value='note'>Note</option>
               <option value='daily_review'>Daily Review</option>
             </select>
-            <input
-              type='time'
-              className='reminder-cell reminder-time'
-              value={dtTime}
-              onChange={(e) => setDtTime(e.target.value)}
-            ></input>
+            <select name='time-hr' className='reminder-cell reminder-type'>
+              <option value='1'>1</option>
+              <option value='2'>2</option>
+              <option value='3'>3</option>
+              <option value='4'>4</option>
+              <option value='5'>5</option>
+              <option value='6'>6</option>
+              <option value='7'>7</option>
+              <option value='8'>8</option>
+              <option value='9'>9</option>
+              <option value='10'>10</option>
+              <option value='11'>11</option>
+              <option value='12'>12</option>
+            </select>
             <div class='reminder-icon'>
               <span class='material-icons'>edit</span>
             </div>
